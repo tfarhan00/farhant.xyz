3f0e7c49bc75bfa11007b7dad4ac116764d1aa99
tengkufarhan00 <farhanalryan2@gmail.com>
2022-12-02 16:51:21 +0700

    disable prerender --all

--- src/routes/api/spotify/+server.js
+++ src/routes/api/spotify/+server.js
@@ -1,7 +1,6 @@
 import { json } from '@sveltejs/kit';
 import { SPOTIFY_ID, SPOTIFY_SECRET, SPOTIFY_TOKEN } from '$env/static/private';
 
-export const prerender = false;
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
