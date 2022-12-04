7a677ba067547debf45b9841ee2a7a6c4e98f730
Tengku Farhan <109069184+tfarhan00@users.noreply.github.com>
2022-12-04 11:05:28 +0700

    Fix: render blocking resources

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,10 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
-		rel="stylesheet"
+		rel="preload"
 	/>
 	<link
-		rel="stylesheet"
+		rel="preload"
 		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
 	/>
 	<meta name="title" content="Tengku Farhan" />
