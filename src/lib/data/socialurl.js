236102dc45d463208c5ba2f03751a6607419a56b
tengkufarhan00 <farhanalryan2@gmail.com>
2022-12-04 13:48:34 +0700

    optimization: compress all image

--- src/lib/data/socialurl.js
+++ src/lib/data/socialurl.js
@@ -4,5 +4,5 @@ export const LINKED_IN = 'https://linkedin.com/in/tengkufarhan';
 export const GITHUB = 'https://github.com/tfarhan00';
 export const READ_CV = 'https://tfarhan.read.cv';
 export const PROFILE_PICT =
-	'https://res.cloudinary.com/dd9nhl1mn/image/upload/v1669482626/fb127cac-3513-4b05-aab8-be404453f110-02-_1__yngkj9.webp';
-export const PROFILE_PICT_2 = `https://res.cloudinary.com/dd9nhl1mn/image/upload/c_scale,w_611/v1669658700/Picsart_22-06-07_15-21-17-204-_2_-_1__1_in8afu.webp`;
+	'https://res.cloudinary.com/dd9nhl1mn/image/upload/c_scale,cs_srgb,f_webp,h_350,q_auto/v1669482626/fb127cac-3513-4b05-aab8-be404453f110-02-_1__yngkj9.webp';
+export const PROFILE_PICT_2 = `https://res.cloudinary.com/dd9nhl1mn/image/upload/c_scale,cs_srgb,f_webp,h_150/v1669658700/Picsart_22-06-07_15-21-17-204-_2_-_1__1_in8afu.webp`;
