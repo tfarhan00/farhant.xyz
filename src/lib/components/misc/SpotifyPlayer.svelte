3629afea6978ff1a9954fc20f623fe92cc04492d
tengkufarhan00 <farhanalryan2@gmail.com>
2022-12-04 17:00:29 +0700

    optimization: add meta tags

--- src/lib/components/misc/SpotifyPlayer.svelte
+++ src/lib/components/misc/SpotifyPlayer.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { Jumper } from 'svelte-loading-spinners';
 	/** @type {any}*/
 	export let song;
 	const theSong = song.data.song;
@@ -7,16 +6,20 @@
 
 <div class="w-full px-4 text-xs py-3 rounded-full bg-black border border-neutral-800">
 	<div class="w-full h-full flex items-center justify-between gap-3">
-		<div class="flex items-center gap-2 overflow-x-auto whitespace-nowrap hideScroll">
-			<div class="">
-				<Jumper size="30" color="#4ADE80" unit="px" duration="2s" />
+		<div class="flex items-center gap-3 overflow-x-auto whitespace-nowrap hideScroll">
+			<div class="shrink-0 w-8 h-8 flex items-center justify-center rounded-full overflow-hidden ">
+				<img
+					src={theSong.coverUrl}
+					alt={theSong.title}
+					class="w-full h-full object-cover"
+				/>
 			</div>
 			<a
-				href={theSong.songUrl}
-				target="_blank"
+			href={theSong.songUrl}
+			target="_blank"
 				rel="noreferrer"
 				class="flex items-center gap-1 hover:text-neutral-500 transition-all duration-300"
-			>
+				>
 				<div>
 					{theSong.title}
 				</div>
@@ -27,12 +30,8 @@
 				<i class="bi bi-arrow-up-right" />
 			</a>
 		</div>
-		<div class="shrink-0 w-8 h-8 flex items-center justify-center rounded-full overflow-hidden ">
-			<img
-				src={theSong.coverUrl}
-				alt={theSong.title}
-				class="w-full h-full object-cover animate-[spin_20s_linear_infinite]"
-			/>
+		<div class="text-green-400 text-xl">
+			<i class="bi bi-spotify"></i>
 		</div>
 	</div>
 </div>
--- src/routes/(app)/journal/[slug]/+page.svelte
+++ src/routes/(app)/journal/[slug]/+page.svelte
@@ -15,6 +15,16 @@
 
 <svelte:head>
 	<title>Farhant - {item.content.journal_title}</title>
+	<meta name="title" content={item.content.journal_title} />
+	<meta
+		name="description"
+		content={item.content.journal_desc}
+	/>
+	<meta
+		name="keywords"
+		content="developer, frontend, react, svelte, blog, designer, javascript, typescript, nextjs, cms, software, engineer, webdevelopment, programming, code"
+	/>
+	<meta name="robots" content="index, follow" />
 </svelte:head>
 
 {#if item}
